81f4eff9f934c05ff5fed5d9c99a3371d362a800
Miquel Yosafat <yosafatmiquel@gmail.com>
2024-06-04 20:37:07 +0700

    ucapan

--- js/undangan.js
+++ js/undangan.js
@@ -38,3 +38,68 @@ window.addEventListener('load', () => {
   }, 1000); // Setiap 1 detik
 
 /* END SCRIPT COUNTDOWN*/
+
+/*SCRIPT COPY TO CLIPBOARD */
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Nomor rekening berhasil disalin!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+/* END SCRIPT*/
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Load ucapan from localStorage
+    loadUcapan();
+
+    document.getElementById('ucapanForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const nama = document.getElementById('nama').value;
+        const ucapan = document.getElementById('ucapan').value;
+
+        const newUcapan = {
+            nama: nama,
+            ucapan: ucapan
+        };
+
+        addUcapan(newUcapan);
+        saveUcapan(newUcapan);
+        document.getElementById('ucapanForm').reset();
+    });
+});
+
+function addUcapan(ucapan, index) {
+    const ucapanCard = document.createElement('div');
+    ucapanCard.className = 'col-md-6 mx-auto card mt-2 p-3';
+    ucapanCard.innerHTML = `
+        <p class="font-normal" style="color: black;"><strong>${ucapan.nama}</strong></p>
+        <p class="font-normal" style="color: black;">${ucapan.ucapan}</p>
+        <button class="btn btn-danger btn-sm" onclick="deleteUcapan(${index})">Hapus</button>
+    `;
+    document.getElementById('ucapanList').appendChild(ucapanCard);
+}
+
+function saveUcapan(ucapan) {
+    let ucapanList = JSON.parse(localStorage.getItem('ucapanList')) || [];
+    ucapanList.push(ucapan);
+    localStorage.setItem('ucapanList', JSON.stringify(ucapanList));
+    loadUcapan();
+}
+
+function loadUcapan() {
+    document.getElementById('ucapanList').innerHTML = '';
+    let ucapanList = JSON.parse(localStorage.getItem('ucapanList')) || [];
+    ucapanList.forEach((ucapan, index) => {
+        addUcapan(ucapan, index);
+    });
+}
+
+function deleteUcapan(index) {
+    let ucapanList = JSON.parse(localStorage.getItem('ucapanList')) || [];
+    ucapanList.splice(index, 1);
+    localStorage.setItem('ucapanList', JSON.stringify(ucapanList));
+    loadUcapan();
+}
